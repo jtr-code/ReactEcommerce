c04d561ae6deb5df5e86ab3df65768a09741911e
jtr-code <jtrtricks@gmail.com>
2022-12-05 21:10:02 +0530

    Added Styles to Home

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 const Home = () => {
-  return (
-    <div><h1>Home</h1></div>
-  )
-}
+    return <Wrapper className="test">Home</Wrapper>;
+};
+
+const Wrapper = styled.section`
+    height: 100vh;
+    background-color: ${({ theme }) => theme.colors.bg};
+`;
 
 export default Home;
