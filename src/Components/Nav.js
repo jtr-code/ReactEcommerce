7a68ba3e8417483a40b6b3b604732a022db167bd
jtr-code <jtrtricks@gmail.com>
2022-12-06 21:12:33 +0530

    [Added] two btn for responsive

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 import { FiShoppingCart } from "react-icons/fi";
+import { CgMenu,CgClose } from "react-icons/cg";
 
 const Nav = () => {
     const Nav = styled.nav`
@@ -170,6 +171,14 @@ const Nav = () => {
                         </NavLink>
                     </li>
                 </ul>
+                {/* Adding two buttons for opening and closing [Responsive] */}
+                <div className="mobile-navbar-btn">
+                    <CgMenu name="menu-outline" className="mobile-nav-icon" />
+                    <CgClose
+                        name="close-outline"
+                        className="mobile-nav-icon close-outline"
+                    />
+                </div>
             </div>
         </Nav>
     );
