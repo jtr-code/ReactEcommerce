e26e0c49ba33fa536086e4681cc56c8b70648e8c
jtr-code <jtrtricks@gmail.com>
2022-12-11 14:29:25 +0530

    [Added] Axios

--- src/context/productcontext.js
+++ src/context/productcontext.js
@@ -1,8 +1,19 @@
-import { createContext, useContext } from "react";
-
+import { createContext, useContext, useEffect } from "react";
+import axios from "axios";
 const AppContext = createContext();
+const API = "https://api.pujakaitem.com/api/products";
 
 const AppProvider = ({ children }) => {
+  const getProducts = async (url) => {
+    const response = await axios.get(url);
+    const products = await response.data;
+    console.log(products);
+  };
+
+  useEffect(() => {
+    getProducts(API);
+  }, []);
+
   return (
     <AppContext.Provider value={{ myName: "AppContext" }}>
       {children}
