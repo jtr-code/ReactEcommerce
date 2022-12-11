43bca7e7380fb413b4704bef0e13d86216c64c7a
jtr-code <jtrtricks@gmail.com>
2022-12-11 13:38:58 +0530

    [Used] Context api in about

--- src/About.js
+++ src/About.js
@@ -1,11 +1,17 @@
-import React from "react";
+import { useProductContextGlobal } from "./context/productcontext";
 import HeroSection from "./components/HeroSection";
 
 const About = () => {
+  const { myName } = useProductContextGlobal();
   const dynamicName = {
-        name:"Thapa Ecommerce"
-    }
-  return <HeroSection myData={dynamicName}/>
+    name: "Thapa Ecommerce",
+  };
+  return (
+    <>
+      {myName}
+      <HeroSection myData={dynamicName} />
+    </>
+  );
 };
 
 export default About;
