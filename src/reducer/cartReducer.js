960c560055dac1799afd70cf4578dc65bc1cd04e
jtr-code <jtrtricks@gmail.com>
2022-12-24 13:34:37 +0530

    [Added] Cart Section

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -1,8 +1,20 @@
 const cartReducer = (state, action) => {
 	switch (action.type) {
-        case "ADD_TO_CART":
-            let {id,color,amount,product}=action.payload
-			console.log('product: ', product);
+		case "ADD_TO_CART":
+			let { id, color, amount, product } = action.payload;
+			console.log("product: ", product);
+
+			let cartProduct = {
+				id: id + color,
+				name: product.name,
+				color,
+				amount,
+				image: product.image[0].url,
+			};
+			return {
+				...state,
+				cart: [...state.cart, cartProduct],
+			};
 
 		default:
 			return state;
