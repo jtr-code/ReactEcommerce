40ae38f2d690d562e5fc38d36f7b7dc69a515051
jtr-code <jtrtricks@gmail.com>
2022-12-26 20:29:46 +0530

    Handled existing error in cart extra stock issue

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -2,21 +2,47 @@ const cartReducer = (state, action) => {
 	switch (action.type) {
 		case "ADD_TO_CART":
 			let { id, color, amount, product } = action.payload;
-			console.log("product: ", product);
+			// console.log("product: ", product);
 
-			let cartProduct = {
-				id: id + color,
-				name: product.name,
-				color,
-				amount,
-				price: product.price,
-				image: product.image[0].url,
-				max: product.stock,
-			};
-			return {
-				...state,
-				cart: [...state.cart, cartProduct],
-			};
+			let existingProduct = state.cart.find((curItem) => {
+				return curItem.id === id + color;
+			});
+			console.log("existingProduct: ", existingProduct);
+
+			if (existingProduct) {
+				let updatedProduct = state.cart.map((curElem) => {
+					if (curElem.id === id + color) {
+						let newAmount = curElem.amount + amount;
+						if (newAmount >= curElem.max) {
+							newAmount=curElem.max
+						}
+						return {
+							...curElem,
+							amount: newAmount,
+						};
+					} else {
+						return curElem;
+					}
+				});
+				return {
+					...state,
+					cart: updatedProduct,
+				};
+			} else {
+				let cartProduct = {
+					id: id + color,
+					name: product.name,
+					color,
+					amount,
+					price: product.price,
+					image: product.image[0].url,
+					max: product.stock,
+				};
+				return {
+					...state,
+					cart: [...state.cart, cartProduct],
+				};
+			}
 
 		case "REMOVE_ITEM":
 			let updatedCart = state.cart.filter(
